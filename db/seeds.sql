b999979ad7e8f06d8592fd013bd07a0745fe3e20
Demetrio <demetrioalmeida@gmail.com>
2020-06-18 23:08:36 -0400

    Homework

---
+++ db/seeds.sql
@@ -0,0 +1,31 @@
+INSERT INTO departament (name) VALUES ("Engineering");
+INSERT INTO departament (name) VALUES ("Sales");
+INSERT INTO departament (name) VALUES ("Legal");
+
+INSERT INTO role (title) VALUES ("Sales Lead");
+INSERT INTO role (title) VALUES ("Salesperson");
+INSERT INTO role (title) VALUES ("Lead Engineer");
+INSERT INTO role (title) VALUES ("Software Engineer");
+INSERT INTO role (title) VALUES ("Accountant");
+INSERT INTO role (title) VALUES ("Laywer");
+INSERT INTO role (title) VALUES ("Legal Team Lead");
+
+INSERT INTO role (salary) VALUES ("100000");
+INSERT INTO role (salary) VALUES ("80000");
+INSERT INTO role (salary) VALUES ("150000");
+INSERT INTO role (salary) VALUES ("120000");
+INSERT INTO role (salary) VALUES ("125000");
+INSERT INTO role (salary) VALUES ("120000");
+INSERT INTO role (salary) VALUES ("190000");
+
+INSERT INTO employee (first_name) VALUES ("John");
+INSERT INTO employee (first_name) VALUES ("Mike");
+INSERT INTO employee (first_name) VALUES ("Sara");
+INSERT INTO employee (first_name) VALUES ("Kevin");
+INSERT INTO employee (first_name) VALUES ("Tom");
+
+INSERT INTO employee (last_name) VALUES ("Doe");
+INSERT INTO employee (last_name) VALUES ("Chan");
+INSERT INTO employee (last_name) VALUES ("Rodriguez");
+INSERT INTO employee (last_name) VALUES ("Allen");
+INSERT INTO employee (last_name) VALUES ("Brown");
